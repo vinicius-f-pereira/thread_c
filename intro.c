f4d7d359ad4de8d5ca2767884234fcef2382e264
nivicius <niviciusdev@gmail.com>
2024-02-23 23:59:25 -0300

    basic to create a threade

--- intro.c
+++ intro.c
@@ -3,7 +3,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void *routine() {
+	printf("test for threads");
+}
 int main(int ac, char **av)
 {
+	pthread_t	t1;
+	pthread_create(&t1, NULL, &routine, NULL);
 	return (0);
 }
